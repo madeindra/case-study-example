8c49c3bb33730db3ed02251ec4c228d9ac9fee7f
Made Indra <made.indra@protonmail.com>
2022-02-09 16:53:11 +0700

    revise bearer middleware

--- backend/middlewares/jwt.js
+++ backend/middlewares/jwt.js
@@ -3,7 +3,7 @@ const secret = process.env.APP_JWT_SECRET || 'secret';
 
 const jwt = async (req, res, next) => {
   // ambil token dari header dengan nama Authorization
-  const token = req.header('Authorization');
+  let token = req.header('Authorization');
 
   // jika token kosong
   if (!token) {
@@ -12,6 +12,18 @@ const jwt = async (req, res, next) => {
       message: 'Unauthorized',
     });
   }
+
+  // jika token tidak diawali dengan 'Bearer '
+  // token JWT tidak wajib menggunakan awalan bearer, hanya jika ingin menerapkan Bearer Token authorization
+  if (!token.startsWith('Bearer ')) {
+    // beri response unauthorized
+    return res.status(401).json({
+      message: 'Unauthorized',
+    });
+  }
+
+  // jika token diawali dengan 'Bearer ', ambil isi setelah awalan tersebut
+  token = token.substring(7, token.length);
 
   // jika token ada, lakukan validasi token menggunakan secret
   jsonwebtoken.verify(token, secret, (err, decoded) => {
