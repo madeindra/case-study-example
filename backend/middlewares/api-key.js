14f98284b5d15e7c4aefdddf02740dbb70e9c74a
Made Indra <made.indra@protonmail.com>
2022-02-09 08:54:03 +0700

    add draft for middleware

--- backend/middlewares/api-key.js
+++ backend/middlewares/api-key.js
@@ -1,6 +1,18 @@
 const secret = process.env.APP_API_SECRET || 'secret';
 
 const apiKey = async (req, res, next) => {
+  // ambil key dari header dengan nama X-API-KEY
+  const key = req.header('X-API-KEY');
+
+  // jika key kosong atau isi key tidak tidak sesuai dengan secret
+  if (!key || key !== secret) {
+    // beri response unauthorized
+    return res.status(401).json({
+      message: 'Unauthorized',
+    });
+  }
+
+  // jika key sesuai isi secret, lanjutkan request ke controller
   next();
 };
 
--- backend/middlewares/jwt.js
+++ backend/middlewares/jwt.js
@@ -1,7 +1,31 @@
+const jsonwebtoken = require('jsonwebtoken');
 const secret = process.env.APP_JWT_SECRET || 'secret';
 
 const jwt = async (req, res, next) => {
-  next();
+  // ambil token dari header dengan nama Authorization
+  const token = req.header('Authorization');
+
+  // jika token kosong
+  if (!token) {
+    // beri response unauthorized
+    return res.status(401).json({
+      message: 'Unauthorized',
+    });
+  }
+
+  // jika token ada, lakukan validasi token menggunakan secret
+  jsonwebtoken.verify(token, secret, (err, decoded) => {
+    // jika terjadi error (karena token tidak valid)
+    if (err) {
+      // beri response unauthorized
+      return res.status(401).json({
+        message: 'Unauthorized',
+      });
+    }
+
+    // jika token valid, lanjutkan request ke controller
+    next();
+  });
 };
 
 module.exports = jwt;
