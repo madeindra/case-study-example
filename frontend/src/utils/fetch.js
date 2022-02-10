04961f9240dde3dbf9f8ebc641c3d0c8a3d94be4
Made Indra <made.indra@protonmail.com>
2022-02-10 11:52:35 +0700

    add fetch utils

---
+++ frontend/src/utils/fetch.js
@@ -0,0 +1,31 @@
+// import axios untuk memanggil API
+const axios = require('axios');
+
+// ambil api key dari environment variable
+const apiKey = process.env.REACT_APP_API_KEY;
+
+async function sendRequest(url = '', method = 'get', body = {}, jwt = null ) {
+  // jika diberikan token JWT, buat header Authorization, jika tidak, buat header X-API-KEY 
+  const authorization = jwt ? {'Authorization': `Bearer ${jwt}` } : {'X-API-KEY': apiKey};
+  
+  // gunakan header authorization & content type
+  const headers = {
+    ...authorization,
+    'Content-Type': 'application/json',
+  }
+
+  // kirim request
+  const response = await axios({
+    url,
+    method,
+    headers,
+    data: JSON.stringify(body), // ubah object jadi json
+  });
+
+  // beri response dalam bentuk json
+  return response.json();
+}
+
+export {
+  sendRequest,
+}
