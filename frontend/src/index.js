81d9da06b70c0477abeecb2c798bfac2b737df9f
Made Indra <made.indra@protonmail.com>
2022-02-10 09:51:30 +0700

    add basic route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+ } from "react-router-dom";
+
 import App from './App';
 
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Todo from './pages/Todo';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='register' element={<Register />} />
+      <Route path='login' element={<Login />} />
+      <Route path='todo' element={<Todo />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ frontend/src/pages/Todo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Todo() {
+  return (
+    <div>
+      <p>Todo</p>
+    </div>
+  );
+}
+
+export default Todo;
