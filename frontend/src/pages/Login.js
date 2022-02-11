47260e4ca409f475fbd30665cbc84186184563c8
Made Indra <made.indra@protonmail.com>
2022-02-11 08:10:36 +0700

    add draft login register page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,50 @@
-import React from "react";
+// import library
+import React, { useState } from 'react';
 
+// import fungsi buatan untuk kirim request
+import { sendRequest } from '../utils/fetch';
+
+// ambil alamat api dari environment variable
+const baseUrl = process.env.REACT_APP_API_URL;
+
+// halaman login
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // kirim form login
+  async function handleLogin(e) {
+    // jangan refresh halaman ketika form disubmit
+    e.preventDefault();
+  
+    // bikin request body
+    const data = {
+      email,
+      password,
+    }
+  
+    // kirim request
+    try {
+      await sendRequest(`${baseUrl}/api/login`, 'post', data);
+    } catch (err) {
+      // jika gagal, tampilkan alert
+      window.alert(err.toString());
+    }
+  }
+
   return (
     <div>
       <p>Login</p>
+
+
+      <form onSubmit={handleLogin}>
+        <input type='text' name='email' onChange={(e) => setEmail(e.target.value)} />
+        <input type='password' name='password' onChange={(e) => setPassword(e.target.value)} />
+
+        <button type='submit'>
+          Login
+        </button>
+      </form>
     </div>
   );
 }
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,9 +1,50 @@
-import React from "react";
+// import library
+import React, { useState } from 'react';
 
+// import fungsi buatan untuk kirim request
+import { sendRequest } from '../utils/fetch';
+
+// ambil alamat api dari environment variable
+const baseUrl = process.env.REACT_APP_API_URL;
+
+// halaman register
 function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // kirim form register
+  async function handleRegister(e) {
+    // jangan refresh halaman ketika form disubmit
+    e.preventDefault();
+  
+    // bikin request body
+    const data = {
+      email,
+      password,
+    }
+  
+    // kirim request
+    try {
+      await sendRequest(`${baseUrl}/api/registration`, 'post', data);
+    } catch (err) {
+      // jika gagal, tampilkan alert
+      window.alert(err.toString());
+    }
+  }
+
   return (
     <div>
       <p>Register</p>
+
+
+      <form onSubmit={handleRegister}>
+        <input type='text' name='email' onChange={(e) => setEmail(e.target.value)} />
+        <input type='password' name='password' onChange={(e) => setPassword(e.target.value)} />
+
+        <button type='submit'>
+          Register
+        </button>
+      </form>
     </div>
   );
 }
