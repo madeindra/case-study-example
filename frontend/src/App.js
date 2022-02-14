70ab5fb1086c7239565076f89f4552c0647317e1
Made Indra <made.indra@protonmail.com>
2022-02-14 08:12:29 +0700

    add link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import { Link } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
         <p>
-          Hello World!
+          Silahkan kunjungi link berikut:
+          <ul>
+            <li>
+              <Link to='register'>Register</Link>
+            </li>
+            <li>
+              <Link to='login'>Login</Link>
+            </li>
+            <li>
+              <Link to='todo'>Lihat Todo</Link>
+            </li>
+            <li>
+              <Link to='todo/create'>Tambah Todo</Link>
+            </li>
+          </ul>
         </p>
     </div>
   );
